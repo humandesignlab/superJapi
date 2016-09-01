9b0bd7408f96dbe56e0b5f223c241af7eaa7a483
Humberto Mangino <hmangino@gmail.com>
2016-09-01 18:29:59 -0500

    Add an alternative image for 404 images as a directive

--- static/js/searchResult.js
+++ static/js/searchResult.js
@@ -1,16 +1,33 @@
 var app = angular.module('testApp', ['angular-toArrayFilter']);
 
+//CONTROLLERS:
 app.controller('searchCtrl', function($scope, $http){
 
-	$http.get('http://127.0.0.1:5000/superJapi/api/v1.0/searchResult')
+	$scope.init = function () {
+    // function implementation
+    	$http.get('http://127.0.0.1:5000/superJapi/api/v1.0/searchResult')
 		.then(function(response){
 			$scope.data = response.data;
 			
-			console.log(response.data.length)
+			/*console.log(response.data.length)
 			for (var i = 0; i < response.data.length; i ++) {
 				console.log(response.data[i]['Tienda']);
-			}
-			
+			}*/			
 		});
+};
+	
+});
 
+
+// DIRECTIVES:
+app.directive('errSrc', function() {
+  return {
+    link: function(scope, element, attrs) {
+      element.bind('error', function() {
+        if (attrs.src != attrs.errSrc) {
+          attrs.$set('src', attrs.errSrc);
+        }
+      });
+    }
+  }
 });
