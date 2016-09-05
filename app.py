01fad4ae2421193a0868d2f1e7dc6715fe4df3e7
Humberto Mangino <hmangino@gmail.com>
2016-09-05 18:44:30 -0500

    Add a post methon on the client side, trying to get it in the backend

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 #!flask/bin/python
 # -*- coding: utf-8 -*-
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
 from searchEngine import searchService
-
+import json
 app = Flask(__name__)
 CORS(app)
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
 
-@app.route('/superJapi/api/v1.0/searchResult', methods=['GET'])
+@app.route('/superJapi/api/v1.0/searchResult', methods=['GET', 'POST'])
 def search():
-	return searchService('papas')
+	data1 = json.loads(request.data.decode())
+	searchString = data1['userInput']
+	return searchService(searchString)
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- static/js/searchResult.js
+++ static/js/searchResult.js
@@ -3,19 +3,34 @@ var app = angular.module('testApp', ['angular-toArrayFilter']);
 //CONTROLLERS:
 app.controller('searchCtrl', function($scope, $http){
 
-	$scope.init = function () {
+	$scope.init = function(input) {
     // function implementation
     	$scope.loaded = false;
-    	$http.get('http://127.0.0.1:5000/superJapi/api/v1.0/searchResult')
-		.then(function(response){
-			$scope.data = response.data;
-			$scope.loaded = true;
-			
-			/*console.log(response.data.length)
-			for (var i = 0; i < response.data.length; i ++) {
-				console.log(response.data[i]['Tienda']);
-			}*/			
-		});
+    		if  (!input) {
+    			return input;
+    		}
+
+    		$http({
+    			url: 'http://127.0.0.1:5000/superJapi/api/v1.0/searchResult',
+    			method: 'POST',
+    			param: {userInput: $scope.input}
+
+    		}).success(function(response){
+    			$scope.data = response.data;
+    			$scope.loaded = true;
+    			console.log(response);
+    		});
+
+	    	/*$http.get('http://127.0.0.1:5000/superJapi/api/v1.0/searchResult')
+			.then(function(response){
+				$scope.data = response.data;
+				$scope.loaded = true;*/
+				
+				/*console.log(response.data.length)
+				for (var i = 0; i < response.data.length; i ++) {
+					console.log(response.data[i]['Tienda']);
+				}		
+		});*/	
 };
 	
 });
