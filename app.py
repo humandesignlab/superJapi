283b10f2d9f529a8ae9e2ad4948288322d143326
Humberto Mangino <hmangino@gmail.com>
2016-09-05 11:44:17 -0500

    Add CORS in app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 #!flask/bin/python
 # -*- coding: utf-8 -*-
 from flask import Flask, jsonify, render_template
+from flask_cors import CORS
 from searchEngine import searchService
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -12,7 +13,7 @@ def index():
 
 @app.route('/superJapi/api/v1.0/searchResult', methods=['GET'])
 def search():
-	return searchService('jamon serrano')
+	return searchService('papas')
 
 if __name__ == '__main__':
 	app.run(debug=True)
