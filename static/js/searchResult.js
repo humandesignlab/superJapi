ab4e340bd6c8815de333b1663803eecc27a263b7
Humberto Mangino <hmangino@gmail.com>
2016-10-10 18:26:56 -0500

    Bootstrap login.html, add key event on input

--- static/js/searchResult.js
+++ static/js/searchResult.js
@@ -48,3 +48,17 @@ app.directive('errSrc', function() {
     }
   }
 });
+
+app.directive('ngEnter', function() {
+        return function(scope, element, attrs) {
+            element.bind("keydown keypress", function(event) {
+                if(event.which === 13) {
+                    scope.$apply(function(){
+                        scope.$eval(attrs.ngEnter, {'event': event});
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    });
